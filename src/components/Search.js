46b2c59e59694f5752b47bbde364879d47f3e1f8
Ezaz Ahmed <ezaz.dev@gmail.com>
2022-09-10 20:05:17 +0600

    setup is ok on android

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,30 @@
-import { TextInput, View } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons'
+import { TextInput, View, StyleSheet } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { elevation } from '../common/styles';
 
 export default function Search() {
   return (
-    <View>
-      <FontAwesome name="search" size="25" />
-      <TextInput placeholder='Restaurants, food' />
+    <View style={[styles.container, styles.elevation]}>
+      <FontAwesome5 name="search" size={24} color="black" />
+      <TextInput style={styles.input} placeholder='Restaurants, food' />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+    marginHorizontal: 25,
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 15,
+    borderRadius: 40,
+  },
+  elevation,
+  input: {
+    fontSize: 20,
+    marginLeft: 10
+  }
+})
